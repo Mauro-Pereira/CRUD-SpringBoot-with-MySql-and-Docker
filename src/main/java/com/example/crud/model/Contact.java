723ac446c8868b546ad70d4ce6f79d28c7573d93
Mauro-Pereira <mauropereira1096@gmail.com>
2021-04-28 15:56:39 -0300

    add annotations in the class

--- src/main/java/com/example/crud/model/Contact.java
+++ src/main/java/com/example/crud/model/Contact.java
@@ -1,8 +1,24 @@
 package com.example.crud.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Contact {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
