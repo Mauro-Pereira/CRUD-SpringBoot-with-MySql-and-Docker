7275cb4ac51615b2e814b3791d0e369409ad4e81
Mauro-Pereira <mauropereira1096@gmail.com>
2021-04-29 18:50:16 -0300

    add save method

--- src/main/java/com/example/crud/controller/ContactController.java
+++ src/main/java/com/example/crud/controller/ContactController.java
@@ -5,6 +5,13 @@ import java.util.List;
 import com.example.crud.model.Contact;
 import com.example.crud.repository.ContactRepository;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +28,40 @@ public class ContactController {
     public List<Contact> findAll(){
         return this.repository.findAll();
     }
+
+    @GetMapping(path = {"/id"})
+    public ResponseEntity<Contact> findById(@PathVariable Long id){
+        return this.repository.findById(id).map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Contact save(@RequestBody Contact contact){
+        return this.repository.save(contact);
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Contact> update(@PathVariable("id") long id,
+                                        @RequestBody Contact contact){
+    return repository.findById(id)
+        .map(record -> {
+            record.setName(contact.getName());
+            record.setEmail(contact.getEmail());
+            record.setPhone(contact.getPhone());
+            Contact updated = repository.save(record);
+            return ResponseEntity.ok().body(updated);
+        }).orElse(ResponseEntity.notFound().build());
+  }
+
+
+    @DeleteMapping(path ={"/{id}"})
+    public ResponseEntity<?> delete(@PathVariable("id") long id) {
+        return repository.findById(id)
+        .map(record -> {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }).orElse(ResponseEntity.notFound().build());
+  }
+
     
 }
