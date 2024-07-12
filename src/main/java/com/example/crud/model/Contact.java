bf83abf9072002438e2d18502ebb8e35964ecc87
Mauro-Pereira <mauropereira@gmail.com>
2024-07-12 14:02:06 -0300

    feat: updating java and spring version in this project

--- src/main/java/com/example/crud/model/Contact.java
+++ src/main/java/com/example/crud/model/Contact.java
@@ -1,5 +1,6 @@
 package com.example.crud.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +9,7 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,11 +18,28 @@ import lombok.NoArgsConstructor;
 public class Contact {
 
     @Id
+    @Column(name="contact_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
+    @Column(name="contact_name")
     private String name;
+
+    @NonNull
+    @Column(name="contact_email")
     private String email;
+
+    @NonNull
+    @Column(name="contact_phone")
     private String phone;
+
+    public Contact(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    
     
 }
--- src/main/java/com/example/crud/repository/ContactRepository.java
+++ src/main/java/com/example/crud/repository/ContactRepository.java
@@ -2,10 +2,14 @@ package com.example.crud.repository;
 
 import com.example.crud.model.Contact;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long>{
+
+    Optional<Contact> findContactByEmail(String email);
     
 }
